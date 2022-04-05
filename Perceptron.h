445301992c250e128f39c6c595817a2e006febbc
Marcin Piotrkowicz <s22222@pjwstk.edu.pl>
2022-04-05 10:28:57 +0200

    added

---
+++ Perceptron.h
@@ -0,0 +1,10 @@
+//
+// Created by s22222 on 2022-04-05.
+//
+
+#ifndef NAI_PROJEKT1_PERCEPTRON_H
+#define NAI_PROJEKT1_PERCEPTRON_H
+class Perceptron{
+    double alfa
+};
+#endif //NAI_PROJEKT1_PERCEPTRON_H
